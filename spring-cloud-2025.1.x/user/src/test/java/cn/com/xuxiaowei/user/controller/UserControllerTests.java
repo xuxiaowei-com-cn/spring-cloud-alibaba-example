657232f914acab86b5bec3f1a71adb1eddeec56f
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2025-12-18 14:18:38 +0800

    :white_check_mark: use java tests

---
+++ spring-cloud-2025.1.x/user/src/test/java/cn/com/xuxiaowei/user/controller/UserControllerTests.java
@@ -0,0 +1,68 @@
+package cn.com.xuxiaowei.user.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class UserControllerTests {
+
+	private static final RestTemplate REST_TEMPLATE = new RestTemplate();
+
+	@LocalServerPort
+	private int port;
+
+	@Test
+	void properties() {
+		String url = "http://localhost:" + port + "/properties";
+		Map<?, ?> map = REST_TEMPLATE.getForObject(url, Map.class);
+		log.info(String.valueOf(map));
+
+		assertNotNull(map);
+
+		assertNotNull(map.get("password"));
+		assertEquals("xuxiaowei.com.cn", map.get("password"));
+
+		assertNotNull(map.get("test.name"));
+		assertEquals("zhangsan", map.get("test.name"));
+	}
+
+	@Test
+	void gatewayServerWebfluxProperties() {
+		String url = "http://localhost:5011/user/properties";
+		Map<?, ?> map = REST_TEMPLATE.getForObject(url, Map.class);
+		log.info(String.valueOf(map));
+
+		assertNotNull(map);
+
+		assertNotNull(map.get("password"));
+		assertEquals("xuxiaowei.com.cn", map.get("password"));
+
+		assertNotNull(map.get("test.name"));
+		assertEquals("zhangsan", map.get("test.name"));
+	}
+
+	@Test
+	void gatewayServerWebmvcProperties() {
+		String url = "http://localhost:5012/user/properties";
+		Map<?, ?> map = REST_TEMPLATE.getForObject(url, Map.class);
+		log.info(String.valueOf(map));
+
+		assertNotNull(map);
+
+		assertNotNull(map.get("password"));
+		assertEquals("xuxiaowei.com.cn", map.get("password"));
+
+		assertNotNull(map.get("test.name"));
+		assertEquals("zhangsan", map.get("test.name"));
+	}
+
+}
