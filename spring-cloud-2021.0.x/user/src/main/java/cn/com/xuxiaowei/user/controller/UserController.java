8c7f6d7e2f008774601a3e24ff7ad5e5f7fa25f1
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2025-12-18 23:48:44 +0800

    :sparkles: spring cloud 2021.0.x

---
+++ spring-cloud-2021.0.x/user/src/main/java/cn/com/xuxiaowei/user/controller/UserController.java
@@ -0,0 +1,58 @@
+package cn.com.xuxiaowei.user.controller;
+
+import cn.com.xuxiaowei.user.properties.TestProperties;
+import cn.com.xuxiaowei.user.properties.UserProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@RestController
+public class UserController {
+
+	private UserProperties userProperties;
+
+	private TestProperties testProperties;
+
+	@Autowired
+	public void setUserProperties(UserProperties userProperties) {
+		this.userProperties = userProperties;
+	}
+
+	@Autowired
+	public void setTestProperties(TestProperties testProperties) {
+		this.testProperties = testProperties;
+	}
+
+	@GetMapping("/properties")
+	public Map<String, Object> getUserProperties() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("name", userProperties.getName());
+		map.put("description", userProperties.getDescription());
+		map.put("username", userProperties.getUsername());
+		map.put("password", userProperties.getPassword());
+		map.put("test.name", testProperties.getName());
+
+		String hostName;
+		try {
+			// Get the local host instance
+			InetAddress localHost = InetAddress.getLocalHost();
+			// Get the computer name (hostname)
+			hostName = localHost.getHostName();
+		}
+		catch (UnknownHostException e) {
+			log.error("获取 IP 异常：", e);
+			hostName = e.getMessage();
+		}
+		map.put("hostName", hostName);
+
+		return map;
+	}
+
+}
---
+++ spring-cloud-2021.0.x/user/src/main/java/cn/com/xuxiaowei/user/properties/TokenProperties.java
@@ -0,0 +1,22 @@
+package cn.com.xuxiaowei.user.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @see RefreshScope 可缺省
+ */
+@Data
+@Component
+@RefreshScope
+@ConfigurationProperties(prefix = "nacos")
+public class TokenProperties {
+
+	/**
+	 *
+	 */
+	private String token;
+
+}
