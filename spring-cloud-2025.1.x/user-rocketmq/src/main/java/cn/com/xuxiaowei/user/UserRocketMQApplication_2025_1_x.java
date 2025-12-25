d2ef9d673a95768a7b98a124f479b3493a573444
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2025-12-26 01:01:44 +0800

    :sparkles: add spring-cloud-2025.1.x/user-rocketmq module

---
+++ spring-cloud-2025.1.x/user-rocketmq/src/main/java/cn/com/xuxiaowei/user/UserRocketMQApplication_2025_1_x.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRocketMQApplication_2025_1_x {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRocketMQApplication_2025_1_x.class, args);
+	}
+
+}
