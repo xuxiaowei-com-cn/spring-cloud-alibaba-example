f0f929fb4ee9de31158801d7ac8e42f6d08ffc65
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2026-04-18 13:56:58 +0800

    :tada: init project

---
+++ src/main/java/cn/com/xuxiaowei/spring_cloud_alibaba_example/SpringCloudAlibabaExampleApplication.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.spring_cloud_alibaba_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudAlibabaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/com/xuxiaowei/spring_cloud_alibaba_example/SpringCloudAlibabaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.xuxiaowei.spring_cloud_alibaba_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
